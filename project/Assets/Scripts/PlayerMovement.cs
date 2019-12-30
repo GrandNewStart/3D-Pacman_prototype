c45b5b000dc5f555b8310f847fd88b6ea92681cf
GrandNewStart <hbjw123@naver.com>
2019-12-30 18:53:10 +0900

    Constructed a maze in which the player can roam about.

---
+++ project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform groundCheck;
+    public LayerMask groundMask;
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float groundDistance = 0.4f;
+    public float jumpHeight = 3f;
+
+    private Vector3 velocity;
+    private bool isGrounded;
+
+    private void Update()
+    {
+        //낙하
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        //이동
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        //점프
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight *  -2f * gravity);
+        }
+    }
+}
